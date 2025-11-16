26d6bf307ff876604132167250af23bd0739fcd7
Takuma TAKAKUSAGI <tak@moove.co.jp>
2025-11-16 21:20:04 +0900

    すべての月で演算できるように修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,7 +1,6 @@
 class CalendarsController < ApplicationController
   def index
-    # 1. 表示する基準日を設定 (URLパラメータがなければ今月) 2025-11-16
-    @date = params[:date] ? Date.parse(params[:date]) : Date.current
+    @date = Date.current
 
     # 2. 今月の最初の日と最後の日を計算
     @start_date = @date.beginning_of_month.beginning_of_week(:sunday) #10-26
@@ -18,13 +17,21 @@ class CalendarsController < ApplicationController
   end
 
   def show
-    puts params['id']
-    # 自分の所属グループの食費系のレシートを取得
-    @receipts = Receipt.joins(:receipt_category)
-      .where(
-      user_group_id: current_user.user_group.id,
-      regist_date: params['id']
-    )
+    # 1. 表示する基準日を設定 (URLパラメータがなければ今月) 2025-11-16
+    @date = params[:id] ? Date.parse(params[:id]) : Date.current
+
+    # 2. 今月の最初の日と最後の日を計算
+    @start_date = @date.beginning_of_month.beginning_of_week(:sunday) #10-26
+    @end_date = @date.end_of_month.end_of_week(:sunday)               #12-06
+
+    # 3. 始まりから終わりまでの全日付の配列を作成
+    @calendar_days = (@start_date..@end_date).to_a #10-26 ~ 12-06
+
+    # 4. 週ごとの表示に役立つよう、日付を7日ずつの配列に分割
+    @weeks = @calendar_days.each_slice(7).to_a
+
+    get_joined_group_receipts_related_food
+    get_joined_group_receipts_except_food
   end
 
   private
