0205082057cd97f3584cebe0f58b81a3cdb66826
Takuma TAKAKUSAGI <tak@moove.co.jp>
2025-11-16 19:16:59 +0900

    カレンダーに金額合計表示

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,28 +1,62 @@
 class CalendarsController < ApplicationController
   def index
-    # 1. 表示する基準日を設定 (URLパラメータがなければ今月)
+    # 1. 表示する基準日を設定 (URLパラメータがなければ今月) 2025-11-16
     @date = params[:date] ? Date.parse(params[:date]) : Date.current
 
     # 2. 今月の最初の日と最後の日を計算
-    @start_date = @date.beginning_of_month.beginning_of_week(:sunday)
-    @end_date = @date.end_of_month.end_of_week(:sunday)
+    @start_date = @date.beginning_of_month.beginning_of_week(:sunday) #10-26
+    @end_date = @date.end_of_month.end_of_week(:sunday)               #12-06
 
     # 3. 始まりから終わりまでの全日付の配列を作成
-    @calendar_days = (@start_date..@end_date).to_a
+    @calendar_days = (@start_date..@end_date).to_a #10-26 ~ 12-06
 
     # 4. 週ごとの表示に役立つよう、日付を7日ずつの配列に分割
     @weeks = @calendar_days.each_slice(7).to_a
 
-    # 食費の合計
-    # ハッシュで持っておくのが良さそう
-    @receipt = Receipt.where("user_id = ?", current_user.id)
-    # グループの集計結果を出したいから、レシートはグループidに紐づけるべき？
-
-
-    #
-    # 食費以外の合計
+    get_joined_group_receipts_related_food
+    get_joined_group_receipts_except_food
   end
 
   def show
+  end
+
+  private
+
+  def get_joined_group_receipts_related_food
+    joined_group_receipts_related_food = Receipt.joins(:receipt_category)
+      .where(
+      user_group_id: current_user.user_group.id,
+      receipt_category: {food_related: true}
+    )
+
+    @calendar_price_related_food = {}
+    @calendar_days.each do |day|
+      sum = 0
+      joined_group_receipts_related_food.each do |receipt|
+        if day == receipt.regist_date
+          sum += receipt.price
+        end
+      end
+      @calendar_price_related_food[day] = sum
+    end
+  end
+
+  def get_joined_group_receipts_except_food
+    joined_group_receipts_except_food = Receipt.joins(:receipt_category)
+      .where(
+      user_group_id: current_user.user_group.id,
+      receipt_category: {food_related: false}
+    )
+
+    @calendar_price_except_food = {}
+    @calendar_days.each do |day|
+      sum = 0
+      joined_group_receipts_except_food.each do |receipt|
+        if day == receipt.regist_date
+          sum += receipt.price
+        end
+      end
+      @calendar_price_except_food[day] = sum
+    end
   end
 end
