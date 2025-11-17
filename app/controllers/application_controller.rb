32e237462086a0f7def33e6a27627484e1d21830
taku-enginner <taku.enginner@gmail.com>
2025-11-17 13:13:44 +0900

    dev環境でエラーを正常に捕まえるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,23 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
 
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from StandardError, with: :render_500
+  #rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  #rescue_from ActionController::RoutingError, with: :render_404
+  #rescue_from StandardError, with: :render_500
 
-  def render_404(exception = nil)
-    if exception
-      logger.info "Rendering 404 with exception: #{exception.message}"
-    end
-    render template: "errors/error_404", status: 404, layout: application
-  end
+  #def render_404(exception = nil)
+    #if exception
+      #logger.info "Rendering 404 with exception: #{exception.message}"
+    #end
+    #render template: "errors/error_404", status: 404, layout: 'application'
+  #end
 
-  def render_500(exception = nil)
-    if exception
-      logger.info "Rendering 500 with exception: #{exception.message}"
-    end
-    render template: "errors/error_500", status: 500, layout: application
-  end
+  #def render_500(exception = nil)
+    #if exception
+      #logger.info "Rendering 500 with exception: #{exception.message}"
+    #end
+    #render template: "errors/error_500", status: 500, layout: 'application'
+  #end
 end
