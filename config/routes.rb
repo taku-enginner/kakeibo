a34c35abc3ddcf555749a6cf6468e68eba5622d3
taku-enginner <taku.enginner@gmail.com>
2025-11-17 13:43:47 +0900

    カレンダーの月移動ボタンにCSSを当てる

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   }
   resources :receipt_categories, only: [:index, :show, :create, :edit, :update, :destroy]
   resources :receipts
-  resources :calendars, only: [:index, :show]
+  resources :calendars, only: [:show]
   resources :daily_results, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
