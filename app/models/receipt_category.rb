61e0a3f8d87e83821cf892215255b2d520e8816b
taku-enginner <taku.enginner@gmail.com>
2025-11-18 20:13:39 +0900

    descriptionを空欄可に

--- app/models/receipt_category.rb
+++ app/models/receipt_category.rb
@@ -1,6 +1,5 @@
 class ReceiptCategory < ApplicationRecord
   has_many :receipts
   validates :name, presence: true
-  validates :description, presence: true
   validates :food_related, presence: true
 end
